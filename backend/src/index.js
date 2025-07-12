4efcf934a102ea4c2234e743652158c6976ecbdb
fardanio19 <farhatmoh2000@gmail.com>
2025-07-12 16:12:15 +0700

    fix keempat

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" })); 
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://skripsi-insyaallah.vercel.app/",
     credentials:true
 }))
 
