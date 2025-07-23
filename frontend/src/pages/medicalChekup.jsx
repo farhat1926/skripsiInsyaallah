227191cc19c094924cfbb2cc268037aa1c500c10
fardanio19 <farhatmoh2000@gmail.com>
2025-07-23 11:03:17 +0700

    pengumpulan home12

--- frontend/src/pages/medicalChekup.jsx
+++ frontend/src/pages/medicalChekup.jsx
@@ -33,13 +33,7 @@ return (
     </div>
 
     {/* Image Side */}
-    <div className="mt-10 md:mt-0 md:w-1/2 flex justify-center">
-      <img
-        src="/home.png"
-        alt="Dokter"
-        className="w-72 h-96 object-cover rounded-2xl shadow-2xl hover:scale-105 transition duration-500 ease-in-out"
-      />
-    </div>
+    
   </div>
     </section>
 
